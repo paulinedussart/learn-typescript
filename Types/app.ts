b2071495b38b16d5461600326b08af8e26eb3301
Pauline Dussart <paulinedussart04@gmail.com>
2022-07-20 14:47:33 +0200

    literal types

--- Types/app.ts
+++ Types/app.ts
@@ -17,3 +17,26 @@ console.log(combinedResult2)
 
 
 // Literal Types
+
+function combineLiteralTypes(number1: number |string, number2: number | string, resultConversion: "as-number" | "as-text") {
+  let result
+  if (typeof number1 == "number" && typeof number2 == "number" ) {
+   result = number1 + number2
+  } else {
+    result = number1.toString() + number2.toString()
+  }
+  // if (resultConversion === 'as-number') {
+  //   return +result;
+  // } else {
+  //   return result.toString();
+  // }
+}
+
+const combinedAges = combineLiteralTypes(30, 26, 'as-number');
+console.log(combinedAges);
+
+const combinedStringAges = combineLiteralTypes('30', '26', 'as-number');
+console.log(combinedStringAges);
+
+const combinedNames = combineLiteralTypes('Max', 'Anna', 'as-text');
+console.log(combinedNames);
